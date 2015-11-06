9b76bec7700c201230b8f2fa8d9ecc229cf3d360
Felipe Menanteau <felipe@illinois.edu>
2015-11-06 14:56:50 +0000

    Added setup.py installation file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*.py") + glob.glob("bin/*.txt")
+
+# The main call
+setup(name='despyfitsutils',
+      version ='0.1.0',
+      license = "GPL",
+      description = "A set of handy Python fitsfile-related utility functions for DESDM",
+      author = "Felipe Menanteau, Michelle Gower",
+      author_email = "felipe@illinois.edu",
+      packages = ['despyfitsutils'],
+      package_dir = {'': 'python'},
+      scripts = bin_files,
+      data_files=[('ups',['ups/despyfitsutils.table']),]
+      )
+
